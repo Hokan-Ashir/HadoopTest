eac51335e09691a9d3405eca88eaf6d9873db199
hokan <achkasov.anton.92@gmail.com>
2016-07-13 21:11:52 +0300

    divided project to separate homework MAVEN modules, with stubs from2 to 5 and some pretty simple, but a big ugly example code from https://habrahabr.ru/post/189798/, old code removed

--- Homework1/execute-job.sh
+++ Homework1/execute-job.sh
@@ -9,6 +9,11 @@ do
 done
 echo "NodeManager started. Hadoop cluster initialized. Awaiting 30 sec. to get NameNode come from SafeMode state ..."
 sleep 30
-echo "NameNode came from SafeMode state, running job ..."
+echo "NameNode came from SafeMode state"
+echo "Staring history server"
+./sbin/mr-jobhistory-daemon.sh start historyserver
+echo "History server started"
+echo "Running a job ..."
 #bin/hadoop jar share/hadoop/mapreduce/hadoop-mapreduce-examples-2.7.1.jar grep input output 'dfs[a-z.]+'
-bin/hadoop jar /opt/hadoop-examples.jar
+bin/hadoop jar /opt/homework-1.jar Grep input output 'dfs[a-z.]+'
+echo "MR job has finished"
