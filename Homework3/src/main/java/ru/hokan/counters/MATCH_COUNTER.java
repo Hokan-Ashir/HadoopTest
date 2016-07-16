0e2b5e1061795441103ca90ed2b883ec9ba51b01
hokan <achkasov.anton.92@gmail.com>
2016-07-17 02:37:05 +0300

    HW3 added counters (count _number_ of IE/Mozzila/Other browsers)

---
+++ Homework3/src/main/java/ru/hokan/counters/MATCH_COUNTER.java
@@ -0,0 +1,7 @@
+package ru.hokan.counters;
+
+public enum MATCH_COUNTER {
+    IE_BROWSER_COUNTER,
+    MOZZILA_BROWSER_COUNTER,
+    OTHER_BROWSER_COUNTER
+}
--- Homework3/src/main/java/ru/hokan/custom/IpDataCountMapper.java
+++ Homework3/src/main/java/ru/hokan/custom/IpDataCountMapper.java
@@ -1,8 +1,12 @@
 package ru.hokan.custom;
 
+import eu.bitwalker.useragentutils.Browser;
+import eu.bitwalker.useragentutils.UserAgent;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import ru.hokan.counters.BrowsersList;
+import ru.hokan.counters.MATCH_COUNTER;
 
 import java.io.IOException;
 
@@ -21,6 +25,16 @@ public class IpDataCountMapper extends Mapper<LongWritable, Text, Text, AverageT
         String input = valueIn.toString();
         String[] split = input.split("\\s");
         String ipName = split[POSITION_IP_ADDR_IN_STRING];
+
+        UserAgent userAgent = UserAgent.parseUserAgentString(input);
+        Browser browser = userAgent.getBrowser();
+        if (BrowsersList.INSTANCE.IE_BROWSERS.contains(browser)) {
+            context.getCounter(MATCH_COUNTER.IE_BROWSER_COUNTER).increment(1);
+        } else if (BrowsersList.INSTANCE.FIREFOX_BROWSERS.contains(browser)) {
+            context.getCounter(MATCH_COUNTER.MOZZILA_BROWSER_COUNTER).increment(1);
+        } else {
+            context.getCounter(MATCH_COUNTER.OTHER_BROWSER_COUNTER).increment(1);
+        }
 
         String numberOfBytesStringPart = split[POSITION_NUMBER_OF_BYTES_IN_STRING];
         if (numberOfBytesStringPart.equals("-")) {
