8dff6a9f97b278b7f4f16fd815ec5e39ad9d3a85
hokan <achkasov.anton.92@gmail.com>
2016-07-18 00:02:18 +0300

    HW3 added snappy support it is used to be error during docker image creation, so I abandoned it and used HDP build to get Snappy see https://github.com/sequenceiq/hadoop-docker/issues/47, second comment for more info and be ware =)

--- Homework3/src/main/java/ru/hokan/custom/IpBytesCounter.java
+++ Homework3/src/main/java/ru/hokan/custom/IpBytesCounter.java
@@ -4,6 +4,8 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.SnappyCodec;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.NLineInputFormat;
@@ -32,6 +34,8 @@ public class IpBytesCounter extends Configured implements Tool {
     public int run(String[] args) throws Exception {
         Configuration config = new Configuration();
         config.set("mapred.textoutputformat.separator", ",");
+        config.setBoolean("mapred.output.compress", true);
+        config.setClass("mapred.output.compression.codec", SnappyCodec.class, CompressionCodec.class);
         config.setInt(NLineInputFormat.LINES_PER_MAP, 1000);
 
         Job job = Job.getInstance(config);
