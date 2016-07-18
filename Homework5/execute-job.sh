28178d86c554803ac4906e921fe7c8dbf8c02727
Anton_Achkasov <achkasov.anton.92@gmail.com>
2016-07-18 19:20:03 +0300

    HW5 stub files commit

---
+++ Homework5/execute-job.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd $HADOOP_PREFIX
+
+while [ -z `jps | grep NodeManager` ]
+do
+    echo "Waiting for NodeManager to start ..."
+    sleep 3
+done
+
+echo "NodeManager started. Hadoop cluster initialized. Awaiting 30 sec. to get NameNode come from SafeMode state ..."
+sleep 30
+echo "NameNode came from SafeMode state"
+
+echo "Staring history server"
+./sbin/mr-jobhistory-daemon.sh start historyserver
+echo "History server started"
+
+export HADOOP_CLIENT_OPTS="-Xmx4g -Xmn1g -Xms4g $HADOOP_CLIENT_OPTS"
+echo "Running a job ..."
+bin/hadoop jar /opt/homework-5.jar
+echo "MR job has finished"
